bdc74a24544dfc83f1d1eb7c838d287ee5b08ca3
LuisMurSal <al19760547@ite.edu.mx>
2024-09-01 14:28:52 -0700

    mapeo de numeros acomodado

--- roman_number.py
+++ roman_number.py
@@ -1,8 +1,18 @@
 # numeros romanos a valores decimales
 roman_numeral_map = {
-    'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100,
-    'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 
-    'V': 5, 'IV': 4, 'I': 1
+    'M': 1000,
+    'CM': 900,
+    'D': 500,
+    'CD': 400,
+    'C': 100,
+    'XC': 90,
+    'L': 50,
+    'XL': 40,
+    'X': 10,
+    'IX': 9, 
+    'V': 5, 
+    'IV': 4, 
+    'I': 1
 }
 
 def roman_to_int(roman):
